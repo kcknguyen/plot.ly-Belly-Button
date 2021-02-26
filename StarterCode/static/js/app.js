4b10cc37faf45f498007f4d59a0eaf9938e392ac
kcknguyen <71248778+kcknguyen@users.noreply.github.com>
2021-02-26 01:41:27 -0600

    commit again

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,9 +2,7 @@
 // The data from the JSON file is arbitrarily named importedData as the argument
 d3.json("data/samples.json").then((importedData) => {
   console.log(importedData);
-    var data = importedData;
-  
-    //load 
+  let data = importedData;
   function init(){
   var selector = d3.select("#selDataset");
    var nameid = data.names;
@@ -19,32 +17,36 @@ var dataload = data.sample.filter(suject = subjectid === loadnameid);
       return parseFloat(b.sample_values) - parseFloat(a.sample_values);
     });
   
-    // Slice the first 10 objects for plotting
+    // Slice the first 10 objects of sample_values for plotting
     var a_samplevalues = dataload.map(value => value.sample_values(0,10));
-    a_samplevalues = a_samplevalues[0].map.value(value => parseFloat(a.sample_values);
+    a_samplevalues = a_samplevalues[0].map.value(value => parseFloat(a.sample_values));
+    // Reverse the array due to Plotly's defaults
     a_samplevalues = a_samplevalues.reverse();
     console.log(a_samplevalues);
+   
+    // Slice the first 10 objects of the otu_ids for plotting
+    var aotuids = dataload.map(value => value.otu_ids.slice(0,10));
+    var botuids = aotuids[0].map(value => "OTU " + String(value));
+    
+    // Slice the first 10 objects of the otu_labels for plotting
+    var aotu_labels = dataLoad.map(value => value.otu_labels.slice(0,10));
     //value.otu_ids.slice(0, 10));
   
-    // Reverse the array due to Plotly's defaults
-    data = data.reverse();
-  
-    // Trace1 for the Greek Data
-    function init() {
+// Creat trace for chart
     var trace1 = {
-      x: data.map(row => row.greekSearchResults),
-      y: data.map(row => row.greekName),
-      text: data.map(row => row.greekName),
-      name: "bar",
+      x: a_samplevalues ,
+      y: b.otuids,
+      text: a.otu_labels
+      name: "Bar",
       type: "bar",
       orientation: "h"
     };
   
     // data
-    var barchartData = [trace1];
+    var barChartData = [trace1];
   
     // Apply the group bar mode to the layout
-    var layout = {
+    var layout1 = {
       title: "Top 10 OTU",
       xaxis: {
         title: {
@@ -56,4 +58,7 @@ var dataload = data.sample.filter(suject = subjectid === loadnameid);
         }}
 
     };
+    //Render the plot id "bar"
+    Plotly.newPlot("bar", barChartData, layout1);  
 }});
+init();
